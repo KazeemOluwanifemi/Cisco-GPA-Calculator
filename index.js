c034f46b00254d86fa84c1aa6021f480f7a44186
OfficialEseosa <131533377+OfficialEseosa@users.noreply.github.com>
2023-10-08 16:35:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var nameInput = document.getElementById('name').value.trim();
-    var matricNumberInput = document.getElementById('matric-number').value.trim();
-    var departmentInput = document.getElementById('department').value.trim();
-  
-    if (nameInput === '' || matricNumberInput === '' || departmentInput === '') {
-      alert('Please fill in all the required fields.');
-    } else {
-      localStorage.setItem('name', nameInput);
-      window.location.href = 'home.html';
-    }
-  });
-  
+  event.preventDefault();
+
+  var nameInput = document.getElementById('name').value.trim();
+  var matricNumberInput = document.getElementById('matric-number').value.trim();
+  var departmentInput = document.getElementById('department').value.trim();
+
+  if (nameInput === '' || matricNumberInput === '' || departmentInput === '') {
+    alert('Please fill in all the required fields.');
+  } else {
+    // Clear local storage before setting the new name
+    localStorage.clear();
+    localStorage.setItem('name', nameInput);
+    window.location.href = 'home.html';
+  }
+});
